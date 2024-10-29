8161bbf800030953a5d374b6bbd7cb4d0c9da20c
Galangrawr <151451527+Galangrawr@users.noreply.github.com>
2024-10-29 19:39:27 +0700

    first commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,17 +1,44 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
-  public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+export class FolderPage {
+  folder = "Item Management";
+  itemName: string = '';
+  items: string[] = [];
+  isEditing: boolean = false;
+  editingIndex: number | null = null;
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  addItem() {
+    if (this.itemName.trim()) {
+      this.items.push(this.itemName.trim());
+      this.itemName = '';
+    }
+  }
+
+  editItem(index: number) {
+    this.isEditing = true;
+    this.editingIndex = index;
+    this.itemName = this.items[index];
+  }
+
+  updateItem() {
+    if (this.editingIndex !== null && this.itemName.trim()) {
+      this.items[this.editingIndex] = this.itemName.trim();
+      this.cancelEdit();
+    }
+  }
+
+  cancelEdit() {
+    this.isEditing = false;
+    this.editingIndex = null;
+    this.itemName = '';
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
   }
 }
